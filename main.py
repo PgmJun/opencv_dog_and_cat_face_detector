796b304b5169e50945ed0e23637caca63ce3b3d2
PgmJun <chltmdwns96@gmail.com>
2024-01-03 15:12:20 +0900

    feat: 이미지 사이즈 2MB 제한

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from typing import List
 
 import numpy as np
 import cv2
+from starlette.status import HTTP_413_REQUEST_ENTITY_TOO_LARGE
 
 app = FastAPI()
 
@@ -11,15 +12,18 @@ cat_cascade = cv2.CascadeClassifier('model/haarcascade_frontalcatface_extended.x
 
 
 @app.post("/detectDogFace")
-async def detect_dog_face_endpoint(files: List[UploadFile] = File(...)):
+def detect_dog_face_endpoint(files: List[UploadFile] = File(...)):
     results = []
 
     for file in files:
-        contents = await file.read()
+        validate_file_size(file)
+        contents = file.read()
+
         is_dog_face_detected = detect_dog_face(contents)
         results.append(is_dog_face_detected)
 
     return {"results": results}
+
 
 def detect_dog_face(contents):
     # 이미지를 NumPy 배열로 변환
@@ -36,3 +40,12 @@ def detect_dog_face(contents):
     # 감지된 얼굴이 있는지 여부를 반환
 
     return (len(dogs) + len(cats)) > 0
+
+def validate_file_size(file: UploadFile):
+    real = 0
+    for chunk in file.file:
+        real = real + len(chunk)
+        if real > 2 * 1024 * 1024:
+            raise HTTPException(
+                status_code=HTTP_413_REQUEST_ENTITY_TOO_LARGE, detail="Image size is too large"
+            )
